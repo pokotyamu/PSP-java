328afbcc2e9a80adc602699e7063aab049c441a5
pokotyamu <pokotyamu@gmail.com>
2016-01-12 20:01:11 +0900

    必要ないurlのpathを消去

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -31,41 +31,6 @@ public class Main {
         port(Integer.valueOf(System.getenv("PORT")));
         staticFileLocation("/public");
         
-        get("/hello", (req, res) -> "Hello World");
-        
-        get("/", (request, response) -> {
-            Map<String, Object> attributes = new HashMap<>();
-            attributes.put("message", "Hello World!");
-            
-            return new ModelAndView(attributes, "index.ftl");
-        }, new FreeMarkerEngine());
-        
-        get("/db", (req, res) -> {
-            Connection connection = null;
-            Map<String, Object> attributes = new HashMap<>();
-            try {
-                connection = DatabaseUrl.extract().getConnection();
-                
-                Statement stmt = connection.createStatement();
-                stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-                stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-                ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
-                
-                ArrayList<String> output = new ArrayList<String>();
-                while (rs.next()) {
-                    output.add( "Read from DB: " + rs.getTimestamp("tick"));
-                }
-                
-                attributes.put("results", output);
-                return new ModelAndView(attributes, "db.ftl");
-            } catch (Exception e) {
-                attributes.put("message", "There was an error: " + e);
-                return new ModelAndView(attributes, "error.ftl");
-            } finally {
-                if (connection != null) try{connection.close();} catch(SQLException e){}
-            }
-        }, new FreeMarkerEngine());
-        
         post("/mdb/:id", (req,res) -> {
             String uri = "https://psp-analysis.herokuapp.com/mdbs/"+req.params("id")+"/download";
             File file  = JsonMDBParser.create(uri);
